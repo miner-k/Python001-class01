58b6bfce4a61908d58b4a004200cf5ee4b254ff1
minker-k <miner-k@qq.com>
2020-08-23 03:10:00 +0800

    week09

--- week09/myweb/index/form.py
+++ week09/myweb/index/form.py
@@ -1,10 +1,10 @@
 from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField()  # 创建标准的输入框
-    password = forms.CharField(widget=forms.PasswordInput, min_length=6)  # widget设置格式,min_length设置最小长度
+    password = forms.CharField(widget=forms.PasswordInput, min_length=8)  # widget设置格式,min_length设置最小长度
 
 class SigninForm(forms.Form):
     username = forms.CharField()  # 创建标准的输入框
     email = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput, min_length=6)  # widget设置格式,min_length设置最小长度
+    password = forms.CharField(widget=forms.PasswordInput, min_length=8)  # widget设置格式,min_length设置最小长度
 
